77bf1be051f8b01937624cde77690b96424e464d
Miguel-202 <mikytinez@hotmail.com>
2023-02-04 21:30:14 -0500

    MainLobby Done

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private int SceneIndex;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    private float currentSpeed;
+    public bool canMove = true;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+    private Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ProcessInput();
+        if (!canMove)
+        {
+            moveSpeed = 0;
+        }
+        else
+        {
+            moveSpeed = currentSpeed;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+        if (moveDirection.magnitude == 1)
+        {
+            if (moveDirection.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
+        }
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    public float GetSpeed()
+    {
+        return moveSpeed;
+    }
+    public void SetSpeed(float newSpeed)
+    {
+        moveSpeed = newSpeed;
+        Debug.Log(newSpeed);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
