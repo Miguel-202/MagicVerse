7c4b6fea3d38dd8c1c607139651194e242a964e0
Miguel-202 <mikytinez@hotmail.com>
2023-02-04 22:15:40 -0500

    2LevelsDone

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private int SceneIndex;
+    [SerializeField] private Animator anim;
     // Start is called before the first frame update
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneIndex);
+            anim.SetTrigger("Transition");
+            StartCoroutine(ChangeLevel());
         }
+    }
+    IEnumerator ChangeLevel()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(SceneIndex);
     }
 }
